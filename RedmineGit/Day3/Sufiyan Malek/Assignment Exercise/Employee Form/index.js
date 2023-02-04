670b09d1a586ea61a8812bea9596f8b0c4b966e6
sufiyanmalek <sufiyanmalek.geek@gmail.com>
2023-02-04 18:12:26 +0530

    Added Day3

---
+++ RedmineGit/Day3/Sufiyan Malek/Assignment Exercise/Employee Form/index.js
@@ -0,0 +1,31 @@
+var storage;
+function onSubmit(event){
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    storage = {
+        name : document.getElementById('name').value,
+        age : document.getElementById('age').value,
+        gender : document.querySelector('input[name="gender"]:checked').value,
+        design : document.getElementById('design').value,
+        salary : document.getElementById('salary').value,
+        location: document.querySelector('#Location').value,
+        email : document.getElementById('email').value,
+        doj : document.getElementById('doj').value,
+        contactNumber : document.getElementById('contactNumber').value,
+    }
+    var test = JSON.stringify(storage)
+    sessionStorage.setItem(name,test);
+    localStorage.setItem(name,test)
+    
+}
+
+function onRetrieve(event){
+    event.preventDefault();
+    var data = document.getElementById('data');
+    var name = document.getElementById('name').value;
+    var entry = sessionStorage.getItem(name);
+    console.log(JSON.parse(entry));
+    data.innerHTML = '<div>' + entry +'</div>'
+    
+
+}
